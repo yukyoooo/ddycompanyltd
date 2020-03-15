9b01f51a4e21ff90165f2088ac91cf328adcabdc
yukyoooo <45087034+yukyoooo@users.noreply.github.com>
2020-03-15 10:28:16 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+$(function(){
+
+
+  $('.effect-move').css("opacity","0");　//アニメーションの対象となる.effect divと .effect iの表示を消します。
+  $(window).scroll(function (){                 //スクロールした際に.effectごとに、これ以下に記述する内容が実行されます。
+    $(".effect").each(function(){
+      var imgPos = $(this).offset().top;      //変数を指定します。imgPosは、.effectの位置、scrollはスクロールした量、windowHeightはウインドウの高さです。
+      var scroll = $(window).scrollTop();
+      var windowHeight = $(window).height();
+      if (scroll > imgPos - windowHeight + windowHeight/5){ //アニメーションする条件になります。.effectが画面の下から1/5のところまでスクロールする、というのが条件になります。
+        $(".effect-move",this).css({
+          "opacity":"1",
+          "margin-top": "0px"
+        });
+      } else {
+        $(".effect-move",this).css({
+          "opacity":"0",
+          "margin-top":"200px"
+        });
+      }
+    });
+  });
+
+
+});
